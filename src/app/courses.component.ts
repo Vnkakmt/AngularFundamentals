c44cb7dfc8b7c94cb4fd4ec9013b2914388d4ba8
vinayak <vnkakmt@gmail.com>
2022-12-30 20:44:53 +0530

    Basics Services :- ng g s email

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./courses.service";
 
 @Component({
     selector:'courses',
     template:`
-    <h2>{{ getTitle() }}</h2>
+    <h2>{{ title }}</h2>
     <ul>
         <li *ngFor="let course of courses">
         {{ course }}
@@ -14,9 +15,9 @@ import { Component } from "@angular/core";
 })
 export class CoursesComponent{
         title = "List of courses";
-        courses = ["course 1", "course 2",  "course 3"]
+        courses;
 
-        getTitle(){
-            return this.title;
+        constructor(service: CoursesService){
+            this.courses = service.getCourses();
         }
 }

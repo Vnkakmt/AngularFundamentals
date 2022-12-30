d10dd08de2a138c645f505976f7d78c75ce76b07
vinayak <vnkakmt@gmail.com>
2022-12-30 20:26:05 +0530

    Basics of template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'courses',
-    template:'<h2>Courses</h2>'
+    template:`
+    <h2>{{ getTitle() }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+        {{ course }}
+        </li>
+    </ul>
+    
+    `
 })
 export class CoursesComponent{
+        title = "List of courses";
+        courses = ["course 1", "course 2",  "course 3"]
 
+        getTitle(){
+            return this.title;
+        }
 }

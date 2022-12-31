7a57815577c18ba49d5633bdf1a634f6740c4ee4
vinayak <vnkakmt@gmail.com>
2022-12-31 09:06:24 +0530

    Style binding example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,7 +4,7 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector:'courses',
     template:`
-    <button class="btn btn-primary" [class.active]="isActive">Save</button>    
+    <button [style.background]="isActive ? 'blue' : 'white'">Save</button>    
     `
 })
 export class CoursesComponent{
@@ -12,7 +12,7 @@ export class CoursesComponent{
         courses;
         imgUrl = "https://picsum.photos/200/300";
         colSpan = 2;
-        isActive = true;
+        isActive = false;
 
         constructor(service: CoursesService){
             this.courses = service.getCourses();

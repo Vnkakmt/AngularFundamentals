6b6658857516b2f871e1abc706e2e733991bc999
vinayak <vnkakmt@gmail.com>
2022-12-31 11:23:17 +0530

    passing the value from textbox

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,16 +4,13 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector:'courses',
     template:`
-    <input (keyup.enter)="onKeyUp($event)"/>
+    <input #email (keyup.enter)="onKeyUp(email.value)"/>
     `
 })
 export class CoursesComponent{
 
-    onDivClicked(){
-        console.log("Div is called")
-    }
-    onKeyUp($event:any){
-        console.log("Enter was clicked");
+    onKeyUp(email:any){
+        console.log(email);
     
 
     }

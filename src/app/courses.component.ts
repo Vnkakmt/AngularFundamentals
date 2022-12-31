e73c3743e1349db46cb270eb22ea092f66196ce0
vinayak <vnkakmt@gmail.com>
2022-12-31 08:56:14 +0530

    Adding bootstrap:- npm install bootstrap --save

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,18 +4,14 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector:'courses',
     template:`
-    <h2>{{ title }}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-        {{ course }}
-        </li>
-    </ul>
-    
+    <button class="btn btn-primary">Save</button>    
     `
 })
 export class CoursesComponent{
         title = "List of courses";
         courses;
+        imgUrl = "https://picsum.photos/200/300";
+        colSpan = 2;
 
         constructor(service: CoursesService){
             this.courses = service.getCourses();

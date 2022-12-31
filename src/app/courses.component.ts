db21e17e2a046e231b7a19570f8e3ccf1f2ef45c
vinayak <vnkakmt@gmail.com>
2022-12-31 09:12:43 +0530

    method with click event

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,17 +4,13 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector:'courses',
     template:`
-    <button [style.background]="isActive ? 'blue' : 'white'">Save</button>    
+    <button (click)="onSave($event)">Save</button>    
     `
 })
 export class CoursesComponent{
-        title = "List of courses";
-        courses;
-        imgUrl = "https://picsum.photos/200/300";
-        colSpan = 2;
-        isActive = false;
 
-        constructor(service: CoursesService){
-            this.courses = service.getCourses();
-        }
+    onSave($event:any){
+        console.log("Button was clicked", $event);
+    }
+
 }

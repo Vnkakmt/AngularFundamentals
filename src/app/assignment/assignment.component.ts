52bb7a620dc8696c0f3ba1d153530fa504e119f1
vinayak <vnkakmt@gmail.com>
2023-01-02 10:51:04 +0530

    styles:- basics

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -1,21 +1,25 @@
+import { style } from '@angular/animations';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-assignment',
   templateUrl: './assignment.component.html',
-  styleUrls: ['./assignment.component.css']
+  styleUrls: ['./assignment.component.css'],
+  //use below  if line is less for styling
+  // using the below styles makes angular prioritise it first the the .css file
+  styles: [
+    `
+    .test {
+      color: red;
+      background-color:burlywood;
+  }
+    `
+  ]
 })
-export class AssignmentComponent implements OnInit {
+export class AssignmentComponent {
 
   @Input('isFav') isSelected:boolean = false;
   @Output('change') click = new EventEmitter();
-  constructor(){
-
-  }
-  
-  ngOnInit(){
-
-  }
 
   onClick(){
     this.isSelected = !this.isSelected;

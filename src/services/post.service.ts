8bd45305ee580aac87dc3af1381380cfeb2213a3
vinayak <vnkakmt@gmail.com>
2023-01-11 11:05:54 +0530

    Http Services:- add reusable error handling method

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -9,56 +9,52 @@ import { badinput } from 'src/app/bad-input';
 })
 export class PostService {
   private url ='https://jsonplaceholder.typicode.com/posts';
-  private wrongUrl = 'https://jsonplaceholde  .typicode.com/posts'
+  private wrongUrl = 'https://jsonplaceholde.typicode.com/posts'
 
   constructor(private http:HttpClient) { }
 
   getPosts(){
-   return this.http.get(this.url);
+   return this.http.get(this.url)
+   .pipe(
+    catchError(this.handleError)
+   );
   }
 
   createPost(post:any){
     return this.http.post(this.url, JSON.stringify(post))
     .pipe(
       catchError((error:Response)=>{
-        if(error.status === 400)
-        return throwError(()=> new badinput(error.json()))
-        else
-        return throwError(()=> new Error(JSON.stringify(error.json())))
+        this.handleError;
+        return throwError(()=> new AppError(error));
       })
     );
   }
 
   updatePost(post:any){
-    return this.http.patch(this.url +'/'+ post.id, JSON.stringify({isRead:true}));
+    return this.http.patch(this.url +'/'+ post.id, JSON.stringify({isRead:true}))
+    .pipe(
+      catchError(this.handleError))
   }
 
   deletePost(id:any){
     return this.http.delete(this.url + '/'+ id)
     .pipe(
-      catchError((error:Response) =>{
-        console.log(error);
-
-        if(error.status === 404)
-        return throwError(()=> new Error(JSON.stringify(error.json())));
-        else
-        return throwError(()=> new AppError(error))
-      }
-      ))
+      catchError(this.handleError))
     }
 
     deletePostTest(id:any){
       return this.http.delete(this.wrongUrl + '/'+ id)
       .pipe(
-        catchError((error:Response) =>{
-          console.log(error);
-  
-          if(error.status === 404)
-          return throwError(()=> new Error(JSON.stringify(error.json())));
-          else
-          return throwError(()=> new AppError(error))
-        }
-        ))
+        catchError(this.handleError))
       }
 
+      private handleError(error:Response){
+        
+        if(error.status === 400)
+        return throwError(()=> new badinput(error.json()))
+
+        if(error.status == 404)
+          return throwError(()=> new Error('Not Found'));
+        return throwError(()=> new AppError(error));
+      }
 }
